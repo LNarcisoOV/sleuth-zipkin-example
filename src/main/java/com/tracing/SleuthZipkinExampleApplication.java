7c00c9c4fbd6aee4286ab2a29ca0d3b7a9612706
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-05-04 19:06:50 +0100

    First commit.

---
+++ src/main/java/com/tracing/SleuthZipkinExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthZipkinExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthZipkinExampleApplication.class, args);
+	}
+
+}
